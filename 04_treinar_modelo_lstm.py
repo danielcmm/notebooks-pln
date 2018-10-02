3d8e50aa67a7a824f75d0d0db8e3c955daf91668
Daniel <danielcmm@gmail.com>
2018-10-02 12:47:31 -0300

    Removendo sentencas com palavras fora da min_count

--- 04_treinar_modelo_lstm.py
+++ 04_treinar_modelo_lstm.py
@@ -33,7 +33,6 @@ w2v_model = models.Word2Vec.load(caminho_modelo_w2v)
 vocab = list(w2v_model.wv.vocab)
 
 
-# Funcoes que retornam o indice da palavra no vocabulario criado pelo word2vec e vice-versa
 def word2idx(word):
     return w2v_model.wv.vocab[word].index
 
@@ -42,25 +41,13 @@ def idx2word(idx):
     return w2v_model.wv.index2word[idx]
 
 
-maxlen = 15
-sentences = []
-next_tokens = []
+with open('modelos/dados_treino_mincount_10_vagalume.pkl', 'rb') as fp:
+    dados_treino = pickle.load(fp)
 
-for doc in docs_tokenizados:  # Cria as "sentencas" para cada documento do corpus
-    for i in range(0, len(doc) - maxlen):
-        sentences.append(doc[i:i + maxlen])
-        next_tokens.append(doc[i + maxlen])
+x = dados_treino[0]
+y = dados_treino[1]
 
-print('Number of sequences:', len(sentences), "\n")
-
-# Criando os inputs(x) e targets(y)
-x = np.zeros((len(sentences), maxlen), dtype=np.int32)
-y = np.zeros((len(sentences)), dtype=np.int32)
-for i, sentence in enumerate(sentences):
-    for t, token in enumerate(sentence):
-        x[i, t] = word2idx(token)
-    y[i] = word2idx(next_tokens[i])
-
+tamanho_sentencas = 15
 pretrained_weights = w2v_model.wv.vectors
 tamanho_vocab = pretrained_weights.shape[0]
 tamanho_vetor_w2v = pretrained_weights.shape[1]  # 350
@@ -69,7 +56,7 @@ print("Tamanho vocab e w2v vector: ", (tamanho_vocab, tamanho_vetor_w2v))
 units1 = args["units1"]
 units2 = args["units2"]
 learning_rate = args["lr"]
-nome_modelo = "lstm-w2v-wordlevel-{}len-{}-{}-lr-{}sertanejo-vagalume.model".format(maxlen, units1, units2, learning_rate)
+nome_modelo = "lstm-w2v-wordlevel-mincount10-{}-len-{}-{}-lr-{}-sertanejo-vagalume.model".format(tamanho_sentencas, units1, units2, learning_rate)
 caminho_modelo_lstm = "modelos/{}".format(nome_modelo)
 if os.path.isfile(caminho_modelo_lstm):
     print("Carregando modelo lstm previo...")
@@ -94,14 +81,15 @@ else:
 
     model.add(Dense(tamanho_vocab, activation='softmax'))  # Quantidade de 'respostas' possiveis. Tokens neste caso.
     optimizer = Adam(lr=learning_rate)
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"] )
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=["accuracy"])
 
 
 def gerar_texto(epoch, logs):
     print('----- Generating text after Epoch: %d' % epoch)
 
-    start = np.random.randint(0, len(sentences))
-    seed_tokens = list(sentences[start])
+    idx_sentenca = np.random.randint(0, len(x))
+    idx_tokens = list(x[idx_sentenca])
+    seed_tokens = [idx2word(i) for i in idx_tokens]
     print("Seed:")
     print("\"", " ".join(seed_tokens), "\"\n")
     temperatura = args["t"]
@@ -109,7 +97,7 @@ def gerar_texto(epoch, logs):
     aux = 1
     for i in range(500):
 
-        xt = np.zeros((1, maxlen), dtype=np.int32)
+        xt = np.zeros((1, tamanho_sentencas), dtype=np.int32)
 
         for t, token in enumerate(seed_tokens):
             xt[0, t] = word2idx(token)
